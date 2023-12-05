4be04cd8672e22ab94dfd6a42e6cb9457f7ee24f
Duvan Andrade <duvan.andradescd@gmail.com>
2023-12-05 19:40:04 -0300

    se cambio la ruta de las imagenes de data ya que no se estaban mostrando

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,49 +10,49 @@ export const DATA = [
         id: 2,
         name: 'Peperomia Obtusifolia',
         price: 1500,
-        img: '../../public/img/plantasInterior/peperomiaObtusifolia.jpg',
+        img: '/img/plantasInterior/peperomiaObtusifolia.jpg',
         description: `Lo que más destaca de la Peperomia Obtusifolia es que sus hojas son carnosas y es una planta muy resistente y de bajo mantenimiento. Generalmente son planchas chicas utilizadas en mesas o estantes o según la variedad como colgante.`
     },
     {
         id: 3,
         name: 'Oreja de Elefante',
         price: 1500,
-        img: '../../public/img/plantasInterior/orejaDeElefanteAlocacia.jpg',
+        img: '/img/plantasInterior/orejaDeElefanteAlocacia.jpg',
         description: `Se caracteriza por sus grandes hojas de aspecto tropical que se asemejan a la oreja de un elefante, por eso su nombre tan particular. Es una planta que puede adaptarse tanto en interior con buena ventilación como en exterior. En interior se adapta bien solo en lugares con muy buena iluminación y mucha ventilación.`
     },
     {
         id: 4,
         name: 'Ficus Pandurata',
         price: 1500,
-        img: '../../public/img/plantasInterior/ficusPandurata.jpg',
+        img: '/img/plantasInterior/ficusPandurata.jpg',
         description: `Es una planta de hojas muy anchas y extremadamente resistente en condiciones ideales. Resiste la poca luminosidad y es sensible a corrientes de frio y vientos. Es muy utilizada en interiores para dar presencia debido a sus anchas hojas con un verde intenso oscuro. Requiere muy pocos cuidados por eso es una de las favoritas para novatos.`
     },
     {
         id: 5,
         name: 'Alocacia Cucullata',
         price: 1500,
-        img: '../../public/img/plantasInterior/alocaciaCucullata.jpg',
+        img: '/img/plantasInterior/alocaciaCucullata.jpg',
         description: `Exquisita planta de interior que cautivará tus sentidos. Conocida por sus hojas en forma de corazón y su apariencia atractiva, esta planta exótica agregará un toque de sofisticación y naturaleza a sus espacios interiores.`
     },
     {
         id: 6,
         name: 'Monstera Peruviana',
         price: 1500,
-        img: '../../public/img/plantasInterior/monsteraPeruviana.jpg',
+        img: '/img/plantasInterior/monsteraPeruviana.jpg',
         description: `Muy elegida por sus Hojas que presentan orificios en cada una de ellas y la hacen tan particular. Esta es una variedad inusual para cualquier colección de plantas de interior y una excelente manera de agregar interés a cualquier espacio con las clásicas hojas perforadas tan distintivas de las plantas monstera.`
     },
     {
         id: 7,
         name: 'Potus',
         price: 1500,
-        img: '../../public/img/plantasInterior/potus.jpg',
+        img: '/img/plantasInterior/potus.jpg',
         description: `Planta muy utilizada para decorar en estanterías, macetas colgantes o repisas como asi también en escritorios. Suelen ser plantas bastantes resistentes y de hecho son capaces de desarrollarse con facilidad, en casi cualquier lado que se las ubique, y tanto en el exterior de la casa o en el jardín, como en el interior.`
     },
     {
         id: 8,
         name: 'Alocacia Cucullata',
         price: 1500,
-        img: '../../public/img/plantasInterior/alocaciaCucullata.jpg',
+        img: '/img/plantasInterior/alocaciaCucullata.jpg',
         description: `Exquisita planta de interior que cautivará tus sentidos. Conocida por sus hojas en forma de corazón y su apariencia atractiva, esta planta exótica agregará un toque de sofisticación y naturaleza a sus espacios interiores.`
     },
     
