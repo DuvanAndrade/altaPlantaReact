090b873e3e339e15488f094a3fdee0ae498548ad
Duvan Andrade <duvan.andradescd@gmail.com>
2023-12-05 19:41:49 -0300

    data

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react"
 import { pedirDatos } from "../../utilities/utilities"
 import ItemList from "../ItemList/ItemList"
 
-
 const ItemListContainer = ({greeting}) =>{
     
     const [productos, setProdutos] = useState([])
@@ -17,22 +16,18 @@ const ItemListContainer = ({greeting}) =>{
                 setLoading( false )
             })
     }, [])
-
     return (
         <main className="main__container font-serif text-center p-10 text-verdePrincipal font-semibold ">
             
             <div className="text-2xl">
                 <h1>{greeting}</h1>
             </div>
-
             {
                 loading
                     ? <h2 className="text-center text-2xl mt-8">Cargando...</h2>
                     : <ItemList productos={productos}/>
             }
-
         </main>
     )
-
 }
 export default ItemListContainer
